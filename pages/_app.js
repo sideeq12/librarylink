7e16a84a6dbfa1131f6dfc4e81b7ea1355d27f31
sideeq12 <abdwaheed2018@gmail.com>
2022-11-10 09:08:42 +0000

    all pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import Menu from '../components/menuList/menu'
 import Tag from "../components/tag/tag"
 import Welcome from '../components/welcome/welcome'
+import {useRouter } from "next/router"
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+ const HomeChecker = router.route.length
   return <>
-  <Welcome />
+  {HomeChecker > 1 ? <></> : 
+  <Welcome />}
   <div style={{ "display" : "flex"}}>
   <Menu />
   <Component {...pageProps} />
